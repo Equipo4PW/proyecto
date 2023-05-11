a9fe19c7f611f78748b3cc57b4fe9d1fa24b570b
morzalip <pablorm538@gmail.com>
2023-05-11 12:54:34 -0500

    parte 3

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,22 @@ module.exports = {
           test: /\.css$/i,
           use: ["style-loader", "css-loader"],
         },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
       ],
     },
-  
 
   devServer:{
     static: {
