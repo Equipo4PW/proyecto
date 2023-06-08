ee6f608b21a86910ff0af427b1078625c3a5725b
marajennifer12 <lopezlemusmariajennifer@gmail.com>
2023-06-08 12:58:32 -0600

    RandomSearch

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,13 @@
 import css from "../css/styles.css";
 import "bootstrap";
 import "../css/bootstrap.min.css";
+import { container } from "webpack";
 
-const get_meal_btn = document.getElementById("get_meal");
+const Aleatorio = document.getElementById("Random");
+const container = document.getElementById("container");
 
-get_meal_btn.addEventListener("click",() =>{
+
+Aleatorio.addEventListener("click",() =>{
     fetch('https://www.themealdb.com/api/json/v1/1/random.php')
     .then(res => res.json())
     .then(res => {
@@ -14,3 +17,22 @@ get_meal_btn.addEventListener("click",() =>{
         console.warn(e);
     });
 });
+
+const createMeal = (meal) => {
+    const ingredients =[];
+    for (let i=1; i<=20; i++) {
+        if (meal[`strIngredient${i}`]) {
+            ingredients.push (`${meal[`strIngredient${i}`]}` - `${meal[`strMeasure${i}`]}`) //push agrega Al arreglo cada ingrediente escribe cuesta escribiendo así
+             
+        } else {
+            break;
+        }
+
+    }
+    const agregandoComida = 
+    
+    ;
+
+    container.innerHTML = agregandoComida;
+}
+
