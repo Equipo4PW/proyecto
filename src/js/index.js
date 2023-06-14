2ebf2ba9a752314f1fceed48aeb6ae50751c1ffb
marajennifer12 <lopezlemusmariajennifer@gmail.com>
2023-06-14 12:02:13 -0600

    g

--- src/js/index.js
+++ src/js/index.js
@@ -2,32 +2,37 @@ import css from "../css/styles.css";
 import "bootstrap";
 import "../css/bootstrap.min.css";
 import { container } from "webpack";
+import InferAsyncModulesPlugin from "webpack/lib/async-modules/InferAsyncModulesPlugin";
 
 const Aleatorio = document.getElementById("Random");
-const meal_container = document.getElementById("meal_container");
+const contenedor = document.getElementById("meal_container");
 
 Aleatorio.addEventListener("click", () => {
-    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-        .then(res => res.json())
-        .then(res => {
-            createMeal(res.meals[0]);
-        })
-        .catch(e => {
-            console.warn(e);
-        });
+  fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(res => {
+      createMeal(res.meals[0]);
+    })
+    .catch(e => {
+      console.warn(e);
+    });
 });
 
 const createMeal = (meal) => {
-    const ingredients = [];
-    for (let i = 1; i <= 20; i++) {
-        if (meal[`strIngredient${i}`]) {
-            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
-        } else {
-            break;
-        }
+  const ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+    } else {
+      break;
     }
+  }
 
-    const agregandoComida = "Aquí puedes agregar tu estructura HTML para mostrar la comida y los ingredientes";
+  const agregandoComida = 
 
-    meal_container.innerHTML = agregandoComida;
-};
+  <h1 class="strIngredient3"></h1>
+    
+;
+
+  contenedor.innerHTML = agregandoComida;
+}
