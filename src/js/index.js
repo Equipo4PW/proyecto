e7ac6d4e67271bda0cd3fd39233e829e920773b1
marajennifer12 <lopezlemusmariajennifer@gmail.com>
2023-06-16 03:39:48 -0600

    h

--- src/js/index.js
+++ src/js/index.js
@@ -34,3 +34,55 @@ const createMeal = (meal) => {
 
   meal_container.innerHTML = agregandoComida;
 }
+
+//Buscar todo tipo de comida en la barra de búsqueda
+const searchInput = document.getElementById('search');
+const searchButton = document.getElementById('get_meal_btn');
+const results = document.getElementById('results');
+
+searchButton.addEventListener('click', searchMeals);
+
+function searchMeals() {
+  const search = searchInput.value;
+
+  if(search) {
+    const formattedSearch = formatSearchString(search);
+    const url = buildUrl(formattedSearch);
+
+    return getGiphyResults(url)
+      .then(function(gifs) {
+        console.log(gifs)
+        gifs.forEach(function(gif) {
+          const img = document.createElement('img');
+          img.src = gif.images.fixed_height.url;
+          img.alt = gif.title;
+
+          results.appendChild(img)
+        })
+      })
+  }
+}
+
+function formatSearchString(search) {
+  return search.replace(/ /g, '+');
+}
+
+function buildUrl (search) {
+  const API_KEY = 'PMBUpuaj3teq6I6sx3mV6DNlO6znGVfC'; // llave de GIPHY
+  const baseUrl = 'https://api.giphy.com/v1/gifs/search';
+
+  return `${baseUrl}?q=${search}&api_key=${API_KEY}&limit=9`;
+}
+
+function getGiphyResults(url) {
+  return fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      return data.data
+    })
+    .catch(function(err) {
+      console.log(err)
+    })
+}
